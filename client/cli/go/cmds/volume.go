39842e4f7471b1beff839042d001b0e66625d460
bourd0n <andone74@gmail.com>
2018-03-24 00:18:22 +0300

    Commit fixes

--- client/cli/go/cmds/volume.go
+++ client/cli/go/cmds/volume.go
@@ -49,7 +49,7 @@ func init() {
 	volumeCommand.AddCommand(volumeInfoCommand)
 	volumeCommand.AddCommand(volumeListCommand)
 	initGeoRepCommand()
-	initMasterSlaveCommand()
+	//initMasterSlaveCommand()
 
 	volumeCreateCommand.Flags().IntVar(&size, "size", -1,
 		"\n\tSize of volume in GiB")
